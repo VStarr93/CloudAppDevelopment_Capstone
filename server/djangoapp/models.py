43e409a025206ff5334dfe89d26ff1e6b0b294c7
VStarr93 <veronicastarr93@gmail.com>
2022-10-16 20:09:57 -0500

    removed max_length from IntegerField

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -32,7 +32,7 @@ class CarModel(models.Model):
 # - Name
     name = models.CharField(max_length=100)
 # - Dealer id, used to refer a dealer created in cloudant database
-    dealer_id = models.IntegerField(max_length=10)
+    dealer_id = models.IntegerField()
 # - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
     type_choices = (
         ("SEDAN", "Sedan"),
